4054f8eed2b408729e8226a61155651f36285b6e
Alexey Buhayov <neznaykagm@gmail.com>
2023-08-11 22:02:32 +0300

    my-initial-setup

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,4 +1,24 @@
 return {
+  ["NvChad/nvim-colorizer.lua"] = {disable = true},
+  {"prisma/vim-prisma"},
+  {"mattn/emmet-vim"},
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    }
+  },
+  {
+    'numToStr/Comment.nvim',
+    opts = {
+      pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+    },
+    lazy = false,
+  },
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
